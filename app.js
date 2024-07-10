ee95f8ee476a0f40b97b7b941f407a0c409c3c42
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-07-11 00:59:57 +0500

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const login = ()=> {
     
     console.log( window.location.href = 'assest/Login/login.html');
 } 
+    // profile image  upload code 
 
+let image = document.getElementById('profile-pic');
+let showImage = document.getElementById('account-pic');
+
+
+const uploadImage = () => {
+    console.log(showImage)
+    let img = image.files[0];
+    let readFile = new FileReader();
+    readFile.onload = () => {
+        console.log(readFile.result);
+        showImage.src = readFile.result;
+    }
+    readFile.readAsDataURL(img)
+}
 
 
 
